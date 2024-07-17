55397b5b59a5483dec142d01d6d4ab5dbb3887b2
Praveen8160 <praveenp8160@gmail.com>
2024-07-17 14:40:56 +0530

    change product view

--- src/Pages/ProductView.jsx
+++ src/Pages/ProductView.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
+import Loader from "../components/Loader";
 function ProductView() {
   const { id } = useParams();
   const [product, setproduct] = useState([]);
@@ -38,24 +39,30 @@ function ProductView() {
   };
 
   return (
-    <div className="container shadow-xl">
-      <div className="flex flex-col justify-center items-center">
-        <img
-          src={product.image}
-          alt=""
-          className="object-fill mt-7 max-h-96 rounded-2xl"
-        />
-        <h1 className="font-bold text-3xl my-5">{product.title}</h1>
-      </div>
-      <p className="text-lg font-medium mx-20">{product.description}</p>
-      <h2 className="font-bold text-2xl my-5">₹{product.price}</h2>
-      <button
-        className="text-white bg-yellow-900 hover:bg-yellow-700 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 mb-6 lg:py-2.5 mr-2 focus:outline-none"
-        onClick={addToCart}
-      >
-        ADD TO CART
-      </button>
-    </div>
+    <>
+      {product ? (
+        <div className="container shadow-xl">
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src={product.image}
+              alt=""
+              className="object-fill mt-7 max-h-96 rounded-2xl"
+            />
+            <h1 className="font-bold text-3xl my-5">{product.title}</h1>
+          </div>
+          <p className="text-lg font-medium mx-20">{product.description}</p>
+          <h2 className="font-bold text-2xl my-5">₹{product.price}</h2>
+          <button
+            className="text-white bg-yellow-900 hover:bg-yellow-700 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 mb-6 lg:py-2.5 mr-2 focus:outline-none"
+            onClick={addToCart}
+          >
+            ADD TO CART
+          </button>
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 }
 
