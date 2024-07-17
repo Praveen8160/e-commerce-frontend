40d3183e80f3978979a68755186de257913f6791
Praveen8160 <praveenp8160@gmail.com>
2024-07-17 11:31:39 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,116 +26,116 @@ function Header() {
 
   return (
     <header className="shadow sticky z-50 top-0">
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-        <ToastContainer />
-        <div className="flex flex-wrap justify-between items-center max-w-screen-xl">
-          <Link to="/" className="flex items-center">
-            <img src={logo} className="lg:mr-3 h-16" alt="Logo" />
-          </Link>
-          <div className="flex items-center lg:order-2">
-            {isAuthenticated === false ? (
-              <>
-                <Link
-                  to="/SignIn"
-                  className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm lg:px-5 py-2 lg:py-2.5 md:px-5 md:py-2.5 mr-2 focus:outline-none"
-                >
-                  Login
-                </Link>
-                <Link
-                  to="/SignUp"
-                  className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm lg:px-5 py-2 lg:py-2.5 mr-2 md:px-5 md:py-2.5 focus:outline-none"
-                >
-                  Register
-                </Link>
-              </>
-            ) : (
-              <>
-                <Link
-                  className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-1 lg:px-5 py-2 lg:py-2.5 mr-2 md:px-5 md:py-2.5 focus:outline-none"
-                  onClick={Logout}
-                >
-                  Logout
-                </Link>
-              </>
-            )}
-            <Link
-              to="/cart"
-              className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 lg:mr-2 focus:outline-none"
-            >
-              Cart
-            </Link>
-            <button
-              type="button"
-              className="inline-flex items-center p-2 ml-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none "
-              aria-controls="mobile-menu-2"
-              aria-expanded={isMenuOpen ? "true" : "false"}
-              onClick={handleToggleMenu}
-            >
-              <span className="sr-only">Toggle menu</span>
-              {isMenuOpen ? <MdClose size={25} /> : <MdMenu size={25}/>}
-            </button>
-          </div>
-          <div
-            className={`${
-              isMenuOpen ? "block" : "hidden"
-            } justify-between items-center w-full lg:flex lg:w-auto lg:order-1`}
-            id="mobile-menu-2"
+    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+      <ToastContainer />
+      <div className="flex flex-wrap justify-between items-center max-w-screen-xl">
+        <Link to="/" className="flex items-center">
+          <img src={logo} className="lg:mr-3 h-16" alt="Logo" />
+        </Link>
+        <div className="flex items-center lg:order-2">
+          {isAuthenticated === false ? (
+            <>
+              <Link
+                to="/SignIn"
+                className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm lg:px-5 py-2 lg:py-2.5 md:px-5 md:py-2.5 mr-1 focus:outline-none"
+              >
+                Login
+              </Link>
+              <Link
+                to="/SignUp"
+                className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm lg:px-5 py-2 lg:py-2.5 mr-1 md:px-5 md:py-2.5 focus:outline-none"
+              >
+                Register
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link
+                className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-1 lg:px-5 py-2 lg:py-2.5 mr-2 md:px-5 md:py-2.5 focus:outline-none"
+                onClick={Logout}
+              >
+                Logout
+              </Link>
+            </>
+          )}
+          <Link
+            to="/cart"
+            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 lg:mr-2 focus:outline-none"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${
-                      isActive ? "text-orange-700" : "text-gray-700"
-                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                  }
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/Products"
-                  className={({ isActive }) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${
-                      isActive ? "text-orange-700" : "text-gray-700"
-                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                  }
-                >
-                  Product
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/myOrder"
-                  className={({ isActive }) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${
-                      isActive ? "text-orange-700" : "text-gray-700"
-                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                  }
-                >
-                  Order
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/contact"
-                  className={({ isActive }) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${
-                      isActive ? "text-orange-700" : "text-gray-700"
-                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                  }
-                >
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+            Cart
+          </Link>
+          <button
+            type="button"
+            className="inline-flex items-center lg:p-2 p-1 lg:ml-2 md:ml-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none "
+            aria-controls="mobile-menu-2"
+            aria-expanded={isMenuOpen ? "true" : "false"}
+            onClick={handleToggleMenu}
+          >
+            <span className="sr-only">Toggle menu</span>
+            {isMenuOpen ? <MdClose size={25} /> : <MdMenu size={25}/>}
+          </button>
         </div>
-      </nav>
-    </header>
-  );
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } justify-between items-center w-full lg:flex lg:w-auto lg:order-1`}
+          id="mobile-menu-2"
+        >
+          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive ? "text-orange-700" : "text-gray-700"
+                  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Products"
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive ? "text-orange-700" : "text-gray-700"
+                  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                }
+              >
+                Product
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/myOrder"
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive ? "text-orange-700" : "text-gray-700"
+                  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                }
+              >
+                Order
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive ? "text-orange-700" : "text-gray-700"
+                  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                }
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+);
 }
 
 export default Header;
